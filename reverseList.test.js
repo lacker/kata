654a9a4a2bb12302ae3a03e4a64d12e0810fe6f8
Kevin Lacker <lacker@gmail.com>
2017-04-06 10:23:46 -0700

    better test suite

--- reverseList.test.js
+++ reverseList.test.js
@@ -3,3 +3,27 @@ let reverse = require('./reverseList');
 test('reversing null is null', () => {
   expect(reverse(null)).toBeNull();
 });
+
+test('reversing a linked list', () => {
+  let list = {
+    value: 1,
+    next: {
+      value: 2,
+      next: {
+        value: 3,
+        next: null,
+      },
+    },
+  };
+  let goal = {
+    value: 3,
+    next: {
+      value: 2,
+      next: {
+        value: 1,
+        next: null,
+      },
+    },
+  };
+  expect(reverse(list)).toEqual(goal);
+});
