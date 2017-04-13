10609417694c147f9fd86fd336d51093a06d59ad
Kevin Lacker <lacker@gmail.com>
2017-04-13 15:19:19 -0700

    arith

---
+++ arithmetic.js
@@ -0,0 +1,18 @@
+// Evaluates an arithmetic expression.
+// Only handles + and *
+function evaluate(expression) {
+  let parts = expression.split(/[+]/);
+  let answer = 0;
+  for (let part of parts) {
+    let subparts = part.split(/[*]/);
+    let subanswer = 1;
+    for (let subpart of subparts) {
+      subanswer *= subpart;
+    }
+    answer += subanswer;
+  }
+  return answer;
+}
+
+
+// TODO: test
