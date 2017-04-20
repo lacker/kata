545de5eccbd436b4d461caf4145c67b086ff3f91
Kevin Lacker <lacker@gmail.com>
2017-04-20 11:17:36 -0700

    almost palindrome

---
+++ almostPalindrome.js
@@ -0,0 +1,33 @@
+// Checks if this string is a palindrome from index i to j, inclusive.
+function isPalindrome(s, i, j) {
+  if (j <= i) {
+    return true;
+  }
+  if (s[i] !== s[j]) {
+    return false;
+  }
+  return isPalindrome(s, i + 1, j - 1);
+}
+
+// Checks if you can make the string s from i to j, inclusive, into a
+// palindrome by removing at most one character.
+function almostPalindromeHelper(s, i, j) {
+  if (j <= i) {
+    return true;
+  }
+  if (s[i] === s[j]) {
+    return almostPalindromeHelper(s, i + 1, j - 1);
+  }
+
+  // If s[i] and s[j] are different, either s[i] has to be removed, or
+  // s[j] has to be removed.
+  return isPalindrome(s, i + 1, j) || isPalindrom(s, i, j - 1);
+}
+
+function almostPalindrome(s) {
+  return almostPalindromeHelper(s, 0, s.length - 1);
+}
+
+module.exports = almostPalindrome;
+
+// TODO: test
