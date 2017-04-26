b1dbc458a77499271d44c769dd306fb7a5ee1d68
Kevin Lacker <lacker@gmail.com>
2017-04-25 21:25:25 -0700

    tree iterator

---
+++ treeIterator.js
@@ -0,0 +1,32 @@
+class treeIterator {
+  constructor(root) {
+    this.stack = [];
+    while (root) {
+      this.stack.push(root);
+      root = root.left;
+    }
+  }
+
+  endOfPath() {
+    return this.stack[this.stack.length - 1];
+  }
+
+  // Throws an error when there is no next value,
+  // same error as popping from an empty list
+  nextValue() {
+    let current = this.stack.pop();
+    let returnValue = current.value;
+    if (current.right) {
+      let node = current.right;
+      while (node) {
+        this.stack.push(node);
+        node = node.left;
+      }
+    }
+    return returnValue;
+  }
+}
+
+module.exports = treeIterator;
+
+// TODO: test
