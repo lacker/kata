688c6a349c41d140ec63c6b853793cec980830d9
Kevin Lacker <lacker@gmail.com>
2017-05-06 20:39:46 -0700

    sqrt fn

---
+++ sqrt.js
@@ -0,0 +1,14 @@
+function sqrt(x) {
+  let epsilon = 0.0000001;
+  if (x < 0) {
+    throw new Error('cant do sqrt of ' + x);
+  }
+  let a = 0;
+  let b = 1;
+  while (Math.abs(a - b) > epsilon) {
+    a = b;
+    b = (b + x/b) / 2;
+  }
+}
+
+module.exports = sqrt;
