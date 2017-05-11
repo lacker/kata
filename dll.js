8e13d3ed7c7bf644f294c4aa8ac01bb2ccb28713
Kevin Lacker <lacker@gmail.com>
2017-05-11 12:33:41 -0700

    dll class

---
+++ dll.js
@@ -0,0 +1,35 @@
+// A doubly-linked list class.
+// Each item has prev, next, and value.
+class DLL {
+  constructor(value, prev, next) {
+    this.value = value;
+    this.prev = prev || null;
+    this.next = next || null;
+  }
+
+  // Removes this node from a DLL
+  // The prev and next will still be valid after removal.
+  remove() {
+    let prev = this.prev;
+    let next = this.next;
+    if (prev) {
+      this.prev.next = next;
+    }
+    if (next) {
+      this.next.prev = prev;
+    }
+  }
+
+  // Returns a new DLL with this value inserted at the beginning
+  // Can only be called on the head
+  prepend(value) {
+    if (this.prev) {
+      throw new Error('prepend should only be called on the head');
+    }
+    let newNode = new DLL(value, null, this);
+    this.prev = newNode;
+    return newNode;
+  }
+}
+
+module.exports = DLL;
