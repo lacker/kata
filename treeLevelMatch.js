f7ca26ae870da146ded1073ce8a97a3a0128a0bd
Kevin Lacker <lacker@gmail.com>
2017-05-21 15:59:33 -0700

    tree level match

---
+++ treeLevelMatch.js
@@ -0,0 +1,32 @@
+// Returns the next level in a binary tree given the current level
+function nextLevel(level) {
+  let answer = [];
+  for (let node of level) {
+    if (node.left) {
+      answer.push(node.left);
+    }
+    if (node.right) {
+      answer.push(node.right);
+    }
+  }
+  return answer;
+}
+
+// Checks if the two trees have the same contents at each level.
+function levelMatch(tree1, tree2) {
+  let level1 = [tree1];
+  let level2 = [tree2];
+  while (level1.length !== 0 && level2.length !== 0) {
+    if (level1.length !== level2.length) {
+      return false;
+    }
+    for (let i = 0; i < level1.length; i++) {
+      if (level1[i].value !== level2[i].value) {
+        return false;
+      }
+    }
+  }
+  return true;
+}
+
+module.exports = levelMatch;
