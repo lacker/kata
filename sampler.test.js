31a352af07d9d8e1e7dae0ea8ed08d453bda6816
Kevin Lacker <lacker@gmail.com>
2017-05-29 17:07:03 -0700

    wrote test

--- sampler.test.js
+++ sampler.test.js
@@ -13,3 +13,21 @@ function fakeRandom(values) {
     return s;
   }
 }
+
+test('five things', () => {
+  let s = new Sampler();
+  s.add('A', 10);
+  s.add('B', 10);
+  s.add('C', 10);
+  s.add('D', 10);
+  s.add('E', 10);
+  let samples = [
+    s.sample(0.1),
+    s.sample(0.3),
+    s.sample(0.5),
+    s.sample(0.7),
+    s.sample(0.9),
+  ];
+  s.sort();
+  expect(s.join('')).toBe('ABCDE');
+});
