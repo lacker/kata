3dda5ebd17149c4dbbc2aa9294d343ca0388b371
Kevin Lacker <lacker@gmail.com>
2017-05-31 17:55:46 -0700

    little bit

---
+++ maxConnectedSum.js
@@ -0,0 +1,14 @@
+
+// Returns an object with:
+// root: the max connected sum including the root node
+// any: the max connected sum of any subgroup
+function helper(tree) {
+  if (!tree) {
+    return {
+      root: 0,
+      any: 0,
+    };
+  }
+
+  // TODO: write more
+}
