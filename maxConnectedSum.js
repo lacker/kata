c9f57c3a9afc0be7172eaef2a62f0e907b05599e
Kevin Lacker <lacker@gmail.com>
2017-06-01 16:11:03 -0700

    mcs code

--- maxConnectedSum.js
+++ maxConnectedSum.js
@@ -11,5 +11,26 @@ function helper(tree) {
     };
   }
 
-  // TODO: write more
+  let left = helper(tree.left);
+  let right = helper(tree.right);
+
+  // Figure out the max connected sum including the root node
+  let root = tree.value;
+  if (left.root > 0) {
+    root += left.root;
+  }
+  if (right.root > 0) {
+    root += right.root;
+  }
+
+  return {
+    root: root,
+    any: Math.max(left.any, right.any, root),
+  };
 }
+
+function maxConnectedSum(tree) {
+  return helper(tree).any;
+}
+
+module.exports = maxConnectedSum;
