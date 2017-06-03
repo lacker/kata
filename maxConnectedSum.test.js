001449069ca59b41a2fd39872fb25eb6e5f6b7bd
Kevin Lacker <lacker@gmail.com>
2017-06-02 20:53:09 -0700

    some test data

---
+++ maxConnectedSum.test.js
@@ -0,0 +1,25 @@
+const maxConnectedSum = require('./maxConnectedSum');
+
+let tree = {
+  value: 1,
+  left: {
+    value: -2,
+    left: {
+      value: 10,
+    },
+    right: {
+      value: 1,
+    },
+  },
+  right: {
+    value: 10,
+    left: {
+      value: -1,
+    },
+    right: {
+      value: 1,
+    },
+  },
+};
+
+// TODO: test
