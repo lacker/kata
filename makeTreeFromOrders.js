0a52a09eb8bfa177af9e0a3914afb9323b355356
Kevin Lacker <lacker@gmail.com>
2017-06-07 13:23:13 -0700

    wacky code

--- makeTreeFromOrders.js
+++ makeTreeFromOrders.js
@@ -1,3 +1,25 @@
 // Makes a tree from an inorder and a preorder traversal.
+// Assumes all values are distinct
+// Note this could be more efficient with less list-copying
+function makeTree(inorder, preorder) {
+  if (inorder.length !== preorder.length) {
+    throw new Error('inorder length should equal preorder length');
+  }
+  if (inorder.length === 0) {
+    return null;
+  }
+  let rootValue = preorder[0];
+  let index = inorder.indexOf(rootValue);
+  let leftInorder = inorder.slice(0, index);
+  let rightInorder = inorder.slice(index + 1);
+  let leftPreorder = preorder.slice(1, index + 1);
+  let rightPreorder = preorder.slice(index + 2);
+  return {
+    value: rootValue,
+    left: makeTree(leftInorder, leftPreorder),
+    right: makeTree(rightInorder, rightPreorder),
+  };
+}
 
-// TODO
+// TODO: test
+module.exports = makeTree;
