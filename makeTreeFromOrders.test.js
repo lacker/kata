22032c2ad03f010fcebf86bddd03742fab5bcbdb
Kevin Lacker <lacker@gmail.com>
2017-06-09 09:29:06 -0700

    test that doesnt work

--- makeTreeFromOrders.test.js
+++ makeTreeFromOrders.test.js
@@ -1,4 +1,25 @@
 const inorder = require('./inorder');
 const preorder = require('./preorder');
+const makeTree = require('./makeTreeFromOrders');
+const treeEqual = require('./treeEqual');
 
-// TODO: more tests
+let tree = {
+  value: 'A',
+  left: {
+    value: 'B',
+    left: { value: 'C' },
+    right: { value: 'D' },
+  },
+  right: {
+    value: 'E',
+    left: { value: 'F' },
+    right: { value: 'G' },
+  },
+};
+
+test('making the tree', () => {
+  let inorderList = inorder(tree);
+  let preorderList = preorder(tree);
+  let tree2 = makeTree(inorder, preorder);
+  expect(treeEqual(tree, tree2)).toBe(true);
+});
