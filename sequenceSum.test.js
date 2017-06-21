2323f40f24e53608be1313ab0b0f400448c81c51
Kevin Lacker <lacker@gmail.com>
2017-06-21 12:22:46 -0700

    another test

--- sequenceSum.test.js
+++ sequenceSum.test.js
@@ -3,3 +3,7 @@ const sequenceSum = require('./sequenceSum');
 test('a sum that exists', () => {
   expect(sequenceSum([1, 2, 3, 4, 5, 6], 9)).toBe(true);
 });
+
+test('a sum that does not exist', () => {
+  expect(sequenceSum([2, 4, 6, 8, 10, 12, 14, 16, 18, 20], 21)).toBe(false);
+});
