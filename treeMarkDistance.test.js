92c9327aefaa48c37760891f721eb4b2bb7996ec
Kevin Lacker <lacker@gmail.com>
2017-07-06 21:28:36 -0700

    anothe rtest

--- treeMarkDistance.test.js
+++ treeMarkDistance.test.js
@@ -15,3 +15,27 @@ test('cousins', () => {
   };
   expect(treeMarkDistance(tree)).toBe(4);
 });
+
+test('grandparent', () => {
+  let tree = {
+    left: {
+      mark: true,
+      right: {
+        right: {
+          mark: true,
+        }
+      }
+    },
+    right: {
+      left: {
+        left: {},
+        right: {},
+      },
+      right: {
+        left: {},
+        right: {},
+      }
+    }
+  };
+  expect(treeMarkDistance(tree)).toBe(2);
+})
