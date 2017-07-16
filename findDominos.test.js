1f94dc4a8b498f992522f0476a7391cfc9f4b0f9
Kevin Lacker <lacker@gmail.com>
2017-07-15 23:36:45 -0700

    test

--- findDominos.test.js
+++ findDominos.test.js
@@ -4,3 +4,8 @@ test('there is a match', () => {
   let data = [[1, 9], [2, 2], [3, 3], [7, 2], [9, 1], [-5, 15]];
   expect(findDominos(data, 10)).toEqual([[1, 9], [9, 1]]);
 });
+
+test('there is no match', () => {
+  let data = [[1, 2], [3, 4], [5, 5]];
+  expect(findDominos(data, 10)).toEqual(-1);
+});
