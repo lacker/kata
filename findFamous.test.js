f58701d6113864781aed4551f43c8746a163c6f4
Kevin Lacker <lacker@gmail.com>
2017-07-17 22:48:48 -0700

    five

---
+++ findFamous.test.js
@@ -0,0 +1,15 @@
+const findFamous = require('./findFamous');
+
+function fiveIsFamous(i, j) {
+  if (i === 5) {
+    return false;
+  }
+  if (j === 5) {
+    return true;
+  }
+  return (i % 3) === (j % 3) ? true : false;
+}
+
+test('finding five', () => {
+  // TODO
+});
