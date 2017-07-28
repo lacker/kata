35366d4c870d73e2575c12e5ca09f469eced4746
Kevin Lacker <lacker@gmail.com>
2017-07-28 10:59:04 -0700

    more testS

--- subgridCounter.test.js
+++ subgridCounter.test.js
@@ -7,5 +7,7 @@ test('basic subgrid', () => {
     [1, 0, 1],
   ];
   let counter = new SubgridCounter(data);
+  expect(counter.count(0, 0, 0, 0)).toBe(1);
   expect(counter.count(1, 1, 1, 1)).toBe(1);
+  expect(counter.count(0, 1, 2, 2)).toBe(3);
 })
