0d50c197fcb4964a8f09607145a8ea2f9981c482
Kevin Lacker <lacker@gmail.com>
2017-07-29 07:53:05 -0700

    test

--- subgridCounter.test.js
+++ subgridCounter.test.js
@@ -10,4 +10,14 @@ test('basic subgrid', () => {
   expect(counter.count(0, 0, 0, 0)).toBe(1);
   expect(counter.count(1, 1, 1, 1)).toBe(1);
   expect(counter.count(0, 1, 2, 2)).toBe(3);
-})
+});
+
+test('modifying the grid', () => {
+  let data = [[1]];
+  let counter1 = new SubgridCounter(data);
+  expect(counter1.count(0, 0, 0, 0)).toBe(1);
+  data[0][0] = 2;
+  let counter2 = new SubgridCounter(data);
+  expect(counter2.count(0, 0, 0, 0)).toBe(2);
+  expect(counter1.count(0, 0, 0, 0)).toBe(1);  
+});
