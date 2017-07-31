a0852d5cff0cb2ce384263dabe71c1e17794fda1
Kevin Lacker <lacker@gmail.com>
2017-07-31 09:48:24 -0700

    finish helper fn

--- inPlaceShift.js
+++ inPlaceShift.js
@@ -1,12 +1,24 @@
 // Right circular k-shift, in place, starting at i.
 // So this shifts i, i + k, ... ending at i.
 // Returns how many items got shifted.
-function inPlaceShift(array, k, i) {
+function helper(array, k, i) {
   let count = 0;
-  let from = i;
-  let temp = null;
+  let index = i;
+  let carry = array[index];
   while (true) {
-    let to = (from + k) % array.length;
-    // TODO: more
+    index = (index + k) % array.length;
+    let temp = array[to];
+    array[to] = carry;
+    carry = temp;
+    count++;
+    if (index === i) {
+      break;
+    }
   }
+  return count;
+}
+
+// Right circular k-shift everything in the array
+function inPlaceShift(array, k) {
+  // TODO
 }
