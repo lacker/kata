b7fc892149e97d040a8c4faed03165b75e9e56c8
Kevin Lacker <lacker@gmail.com>
2017-08-02 09:46:08 -0700

    test

---
+++ inPlaceShift.test.js
@@ -0,0 +1,7 @@
+const inPlaceShift = require('./inPlaceShift');
+
+test('relatively prime', () => {
+  let arr = [1, 2, 3, 4, 5, 6, 7];
+  inPlaceShift(arr, 3);
+  expect(arr).toEqual([5, 6, 7, 1, 2, 3, 4]);
+};
