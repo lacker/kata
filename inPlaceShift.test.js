08b0569d44fdc93de8da1ea0f680d85e1b35b29b
Kevin Lacker <lacker@gmail.com>
2017-08-04 16:42:34 -0700

    another test

--- inPlaceShift.test.js
+++ inPlaceShift.test.js
@@ -5,3 +5,9 @@ test('relatively prime', () => {
   inPlaceShift(arr, 3);
   expect(arr).toEqual([5, 6, 7, 1, 2, 3, 4]);
 });
+
+test('both even', () => {
+  let arr = [1, 2, 3, 4, 5, 6];
+  inPlaceShift(arr, 2);
+  expect(arr).toEqual([5, 6, 1, 2, 3, 4]);
+});
