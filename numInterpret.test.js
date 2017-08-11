6b505e48c273e6905b829cdae8d280b125cde449
Kevin Lacker <lacker@gmail.com>
2017-08-11 10:40:00 -0700

    testin

--- numInterpret.test.js
+++ numInterpret.test.js
@@ -6,3 +6,14 @@ test('basic functionality', () => {
   expect(numInterpret('111')).toBe(3);
   expect(numInterpret('1111')).toBe(5);
 });
+
+test('stuff that should not work', () => {
+  expect(numInterpret('a')).toBe(0);
+  expect(numInterpret('0123123123')).toBe(0);
+  expect(numInterpret('11113031111')).toBe(0);
+});
+
+test('single pathers', () => {
+  expect(numInterpret('99999999')).toBe(1);
+  expect(numInterpret('39465738946578349657843')).toBe(1);
+});
