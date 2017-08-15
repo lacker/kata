85e2d6aa4e90728c476bfff22abedb44005e4ee6
Kevin Lacker <lacker@gmail.com>
2017-08-15 09:01:38 -0700

    another test

--- arrayCount.test.js
+++ arrayCount.test.js
@@ -3,3 +3,7 @@ const arrayCount = require('./arrayCount');
 test('normal behavior', () => {
   expect(arrayCount([1, 4, 7, 7, 7, 8, 9], 7)).toBe(3);
 });
+
+test('nonexistent', () => {
+  expect(arrayCount([3, 4, 5, 6, 8, 9, 10], 7)).toBe(0);
+});
