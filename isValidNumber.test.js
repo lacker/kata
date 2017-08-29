649f7c967bc49472e3d57eed172a3bb9216d7ddc
Kevin Lacker <lacker@gmail.com>
2017-08-29 09:08:48 -0700

    bad stuff

--- isValidNumber.test.js
+++ isValidNumber.test.js
@@ -3,3 +3,10 @@ const isValidNumber = require('./isValidNumber');
 test('basic', () => {
   expect(isValidNumber('13')).toBe(true);
 });
+
+test('bad stuff', () => {
+  expect(isValidNumber('a')).toBe(false);
+  expect(isValidNumber('.')).toBe(false);
+  expect(isValidNumber('-')).toBe(false);
+  expect(isValidNumber('1e3')).toBe(false);
+});
