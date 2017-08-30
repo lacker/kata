d0c2a4e5e8fefb59d1a7fbc2c2394959d37ac4a2
Kevin Lacker <lacker@gmail.com>
2017-08-30 06:50:43 -0700

    decimals

--- isValidNumber.test.js
+++ isValidNumber.test.js
@@ -9,4 +9,12 @@ test('bad stuff', () => {
   expect(isValidNumber('.')).toBe(false);
   expect(isValidNumber('-')).toBe(false);
   expect(isValidNumber('1e3')).toBe(false);
+  expect(isValidNumber('-.')).toBe(false);
+});
+
+test('decimals', () => {
+  expect(isValidNumber('0.123')).toBe(true);
+  expect(isValidNumber('-0.89898')).toBe(true);
+  expect(isValidNumber('.456')).toBe(true);
+  expect(isValidNumber('-.777')).toBe(true);
 });
