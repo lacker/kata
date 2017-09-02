1954ff1e921c9a29b7ed161f5da7910a02d2afaa
Kevin Lacker <lacker@gmail.com>
2017-09-01 17:12:29 -0700

    start chocie

---
+++ choice.js
@@ -0,0 +1,9 @@
+// Pick k items from the list randomly
+function choice(list, k) {
+  if (list.length > k) {
+    throw new Error(
+      'cannot choose ' + k +
+      ' from a list of length ' + list.length);
+  }
+  // TODO
+}
