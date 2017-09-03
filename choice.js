ee6dc07de6a2e3198bd6569f48446c6f647324e7
Kevin Lacker <lacker@gmail.com>
2017-09-03 15:34:30 -0700

    choice fn

--- choice.js
+++ choice.js
@@ -10,6 +10,16 @@ function choice(list, numToChoose) {
   let output = [];
   while (output.length < numToChoose) {
     // Consider taking index total - 1
-    // TODO
+    // There is a numToChoose / total chance of wanting it
+    let chance = numToChoose / total;
+    if (Math.random() < chance) {
+      // Do select it
+      output.push(list[total - 1]);
+      numToChoose -= 1;
+    }
+    total -= 1;
   }
+  return output;
 }
+
+module.exports = choice;
