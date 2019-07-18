e625ac514e040af239f3fc2f4494af973d9f0c83
Kevin Lacker <lacker@gmail.com>
2019-07-18 08:49:25 -0700

    eval on numbers ok

--- python/lisp.py
+++ python/lisp.py
@@ -1,7 +1,11 @@
 def evaluate(expr):
+	if type(expr) in (int, float):
+		return expr
 	if type(expr) is not list:
 		raise BaseException("can only evaluate lists")
 	return t
 	
-print(evaluate(1))
-print(evaluate(evaluate))
+assert evaluate(1) == 1
+assert evaluate(1.25) == 1.25
+#print(evaluate(evaluate))
+print("ok")
