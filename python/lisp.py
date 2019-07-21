5293cec9f9e9bc3b272a58b44ddb519f974d690b
Kevin Lacker <lacker@gmail.com>
2019-07-21 06:59:25 -0700

    op fn check

--- python/lisp.py
+++ python/lisp.py
@@ -2,9 +2,15 @@ def evaluate(expr):
 	t = type(expr)
 	if t in (int, float):
 		return expr
-	if t is list:
-		return apply(expr[0], expr[1:])
-	raise BaseException(f"cannot evaluate type: {t}")
+	if t is not list:
+		raise BaseException(f"cannot evaluate type: {t}")
+		
+	op = expr[0]
+	args = rest[1:]
+		if type(op) is not function:
+			raise BaseException(f"{op} is not a function")
+	return apply(op, args)
+	
 	
 assert evaluate(1) == 1
 assert evaluate(1.25) == 1.25
