f44adadf544953a9173ca6db9fdbd2e67cb9fd4e
Kevin Lacker <lacker@gmail.com>
2019-07-25 15:57:18 -0700

    add

--- python/lisp.py
+++ python/lisp.py
@@ -11,6 +11,12 @@ def evaluate(expr):
 		raise BaseException(f"{op} is not callable")
 	return op(*args)
 	
+def add(*args):
+	answer = 0
+	for arg in args:
+		answer += arg
+	return answer	
+	
 assert evaluate(1) == 1
 assert evaluate(1.25) == 1.25
 print(evaluate([sum, 1, 2, 3]))
