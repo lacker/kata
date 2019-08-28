527269e068a70a5d48aa68a5cd9cfb82e7da6e5c
Kevin Lacker <lacker@gmail.com>
2019-08-28 16:25:15 -0700

    sip

--- python/almost_palindrome.py
+++ python/almost_palindrome.py
@@ -1,8 +1,16 @@
-def srev(s):
-	return "".join(reversed(s))
-
+# whether s from i to j inclusive is a palindrome
+def seq_is_pal(s, i, j):
+  if i >= j:
+  	return True
+  if s[i] != s[j]:
+  	return False
+  return seq_is_pal(s, i+1, j-1)
+ 
+def is_almost_pal(s, i, j):
+	 return "xxx"
+	 
 def is_pal(s):
-  return s == srev(s)
-  
+	return seq_is_pal(s, 0, len(s)-1)
+    
 print(is_pal("foof"))
 print(is_pal("boof"))
