40380f6512604f48bc9acdf010450b4f3a99c058
Kevin Lacker <lacker@gmail.com>
2019-08-29 09:40:18 -0700

    rust string manip

--- rust/almost_palindrome.rs
+++ rust/almost_palindrome.rs
@@ -1,3 +1,15 @@
+// Whether the sequence from s[i] to s[j] inclusive is a palindrome
+fn sequence_is_palindrome(s: Vec<char>, i:usize, j: usize) -> bool {
+    if i >= j {
+        return true;
+    }
+    if s[i] != s[j] {
+        return false;
+    }
+    return sequence_is_palindrome(s, i + 1, j - 1)
+}
+
 fn main() {
-    println!("Hello, world!");
+    let s = "footatoof";
+    println!("answer = {}", sequence_is_palindrome(s.chars().collect(), 0, 8));
 }
