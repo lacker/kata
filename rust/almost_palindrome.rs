baa5736dc198616fdd8278ed5c4b98850c35e2c6
Kevin Lacker <lacker@gmail.com>
2019-08-31 11:17:35 -0700

    is_palindrome using slices rustily

--- rust/almost_palindrome.rs
+++ rust/almost_palindrome.rs
@@ -1,23 +1,17 @@
 // Whether the sequence from s[i] to s[j] inclusive is a palindrome
-fn sequence_is_palindrome(s: Vec<char>, i:usize, j: usize) -> bool {
-    if i >= j {
+fn is_palindrome(s: &[char]) -> bool {
+    if s.len() < 2 {
         return true;
     }
-    if s[i] != s[j] {
+    let last = s.len() - 1;
+    if s[0] != s[last] {
         return false;
     }
-    return sequence_is_palindrome(s, i + 1, j - 1)
-}
-
-fn is_palindrome(s: &str) -> bool {
-    return sequence_is_palindrome(s.chars().collect(), 0, s.len() - 1);
-}
-
-fn test(s: String, answer: bool) {
-    // TODO
+    is_palindrome(&s[1..last])
 }
 
 fn main() {
     let s = "footatoof";
-    println!("answer = {}", sequence_is_palindrome(s.chars().collect(), 0, 8));
+    let chars: Vec<char> = s.chars().collect();
+    println!("answer = {}", is_palindrome(&chars));
 }
