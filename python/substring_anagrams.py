52735215b51a432960327b7c2c0eef13fb746f3f
Kevin Lacker <lacker@gmail.com>
2019-09-13 14:50:03 -0700

    main f

--- python/substring_anagrams.py
+++ python/substring_anagrams.py
@@ -5,3 +5,9 @@ def update(values, key, delta):
 		values[key] = new_value
 	else:
 		del values[key]
+		
+def find_substring_anagrams(big, small):
+	count = {}
+	for ch in small:
+		update(count, ch, -1)
+	# TODO
