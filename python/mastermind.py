7b239e5f61b6117ca8cae943168c97739b228157
Kevin Lacker <lacker@gmail.com>
2019-12-08 13:41:48 -0800

    something

--- python/mastermind.py
+++ python/mastermind.py
@@ -44,5 +44,6 @@ def right_list(list1, list2):
 def print_score(list1, list2):
 	rp = right_place(list1, list2)
 	ri = right_items(list1, list2)
+	print("ok")
 			
 print(right_items([1,1],[1,2,3,3]))
