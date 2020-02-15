e1279511dc8d7842bb7f546f8a95418411083c84
Kevin Lacker <lacker@gmail.com>
2020-02-15 09:51:27 -0800

    jnfo

--- python/mastermind.py
+++ python/mastermind.py
@@ -50,6 +50,12 @@ def right_answer(list1, list2):
 	s2 = sorted(list2)
 	return s1 == s2
 
+def info(list1, list2):
+	return (
+		right_answer(list1, list2),
+	  right_items(list1, list2),
+	  right_place(list1, list2))
+
 # returns whether you did it	
 def check(list1, list2):
 	if right_answer(list1, list2):
