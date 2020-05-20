61eba4a04139f046c30764dc78127c7c61c0e375
Kevin Lacker <lacker@gmail.com>
2020-05-20 10:03:38 -0700

    a theorem

--- lean/tutorial.lean
+++ lean/tutorial.lean
@@ -1,6 +1,8 @@
 constant n : nat
 
-#reduce (n + 2) - 2
+constants p q : Prop
+
+theorem t1 : p → p := assume hp: p, show p, from hp
 
 /- TODO: prove that times_successor(x) is even -/
 
@@ -8,5 +10,6 @@ def times_successor (n : nat) := n * ( n + 1)
 
 def is_even (a : nat) := ∃ b, 2 * b = a
 
-theorem even_times {a b : nat } (h : is_even a) : is_even (a * b) := sorry
-
+/-
+theorem even_times {a b : nat } (h : is_even a) : is_even (a * b) := sorr
+-/
