d1bd08fd791867c0a78d458fefbeb1f763a12643
Kevin Lacker <lacker@gmail.com>
2020-05-21 16:01:17 -0700

    some theorems

--- lean/tutorial.lean
+++ lean/tutorial.lean
@@ -2,7 +2,18 @@ constant n : nat
 
 constants p q : Prop
 
-theorem t1 : p → p := assume hp: p, show p, from hp
+example : p → p := assume hp: p, show p, from hp
+
+theorem or_swap (p q : Prop) : p ∨ q → q ∨ p := assume h: p ∨ q,
+or.elim
+h
+(assume hp: p, show q ∨ p, from or.intro_right q hp)
+(assume hq: q, show q ∨ p, from or.intro_left p hq)
+
+example : p ∨ q ↔ q ∨ p :=
+iff.intro
+(or_swap p q)
+(or_swap q p)
 
 /- TODO: prove that times_successor(x) is even -/
 
