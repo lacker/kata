92f37ef68e06d6a39de3b5d4143ba4d77905e8c5
Kevin Lacker <lacker@gmail.com>
2020-05-22 11:42:46 -0700

    some mathematical things

--- lean/tutorial.lean
+++ lean/tutorial.lean
@@ -1,12 +1,10 @@
-constant n : nat
-
 constants p q : Prop
 
 example : p → p := assume hp: p, show p, from hp
 
-theorem or_swap (p q : Prop) : p ∨ q → q ∨ p := assume h: p ∨ q,
-or.elim
-h
+theorem or_swap (p q : Prop) : p ∨ q → q ∨ p :=
+assume h: p ∨ q,
+or.elim h
 (assume hp: p, show q ∨ p, from or.intro_right q hp)
 (assume hq: q, show q ∨ p, from or.intro_left p hq)
 
@@ -16,11 +14,37 @@ iff.intro
 (or_swap q p)
 
 /- TODO: prove that times_successor(x) is even -/
+def times_successor (n : ℕ) := n * ( n + 1)
 
-def times_successor (n : nat) := n * ( n + 1)
+def is_even (a : ℕ) := ∃ b, 2 * b = a
 
-def is_even (a : nat) := ∃ b, 2 * b = a
+def is_odd (a : ℕ) := ∃ b, 2 * b + 1 = a
 
-/-
-theorem even_times {a b : nat } (h : is_even a) : is_even (a * b) := sorr
--/
+theorem zero_is_even: is_even 0 :=
+exists.intro 0 (zero_mul 2)
+
+theorem even_plus_one_is_odd (a : ℕ) (h: is_even a) : is_odd (a + 1) :=
+exists.elim h
+(assume b, assume hb: 2 * b = a, show is_odd (a + 1), from
+exists.intro b
+  (calc
+  2 * b + 1 = a + 1 : by rw hb
+  )       
+)
+
+lemma l1: 2 * 1 = 1 + 1 := rfl
+
+theorem odd_plus_one_is_even (a : ℕ) (h: is_odd a) : is_even (a + 1) :=
+exists.elim h
+(assume b, assume hb: 2 * b + 1 = a, show is_even (a + 1), from
+exists.intro (b + 1)
+(calc
+  2 * (b + 1) = 2 * b + 2 * 1 : by rw mul_add
+  ... = 2 * b + 1 + 1 : by rw l1
+  ... = a + 1 : by rw hb
+)
+)
+
+/- TODO: get library_search working, try to avoid using this stupid lemma -/
+
+
