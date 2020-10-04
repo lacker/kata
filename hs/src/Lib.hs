0bc7ba236c5236310920711accab6e11848ceafd
Kevin Lacker <lacker@gmail.com>
2020-10-04 13:59:10 -0700

    part of fizzbuzz

--- hs/src/Lib.hs
+++ hs/src/Lib.hs
@@ -2,5 +2,13 @@ module Lib
     ( someFunc
     ) where
 
+helper :: Int -> String
+
+helper n
+    | n `mod` 15 == 0 = "FizzBuzz"
+    | n `mod` 5 == 0 = "Buzz"
+    | n `mod` 3 == 0 = "Fizz"
+    | otherwise = show n
+
 someFunc :: IO ()
 someFunc = putStrLn "Hello Haskell World!"
