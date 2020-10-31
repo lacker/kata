0c39ab0fe383fec8f914dc7a27a6a10bcd07ed3b
Kevin Lacker <lacker@gmail.com>
2020-10-31 12:07:26 -0700

    tree

--- python/unification.py
+++ python/unification.py
@@ -5,9 +5,15 @@ class Constant:
 		self.token = token
 
 class Variable:
-	sef __init__(self, variable_id):
+	def __init__(self, variable_id):
 		self.variable_id = variable_id
-		
+	
+class Tree:
+	def __init__(self, left, right):
+		self.left = left
+		self.right = right
+			
+				
 V = Variable
 C = Constant
 
