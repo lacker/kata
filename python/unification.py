81f44f454562bcc1993f149d7c470a54ae786b6c
Kevin Lacker <lacker@gmail.com>
2020-11-01 11:26:42 -0800

    expr

--- python/unification.py
+++ python/unification.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python
 
+
+class Expression:
+	def __init__(self, token=None, left=None, right=None, variable_id=None):
+		self.token = token
+		self.left = left
+		self.right = right
+		self.variable_id = variable_id
+
 class Constant:
 	def __init__(self, token):
-		self.token = token
+		self.token =
 
 class Variable:
 	def __init__(self, variable_id):
