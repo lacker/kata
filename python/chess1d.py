d71de100403c7c5a1c3a40b1fe56039d4d602781
Kevin Lacker <lacker@gmail.com>
2021-06-18 13:48:04 -0700

    legal_moves

--- python/chess1d.py
+++ python/chess1d.py
@@ -4,3 +4,10 @@ One-dimensional chess. See https://gumroad.com/l/1DChess
 """
 
 START = "KQRBNP....pnbrqk"
+
+def legal_moves(board):
+  """
+  Moves are represented as a (i, j) tuple, where the piece is moving from board[i] to board[j].
+  """
+  for i, piece in enumerate(board):
+    pass
