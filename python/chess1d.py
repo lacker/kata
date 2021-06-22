001bed5af9df18746a426a0bac2a985e4719b8f1
Kevin Lacker <lacker@gmail.com>
2021-06-21 17:06:35 -0700

    color

--- python/chess1d.py
+++ python/chess1d.py
@@ -5,6 +5,12 @@ One-dimensional chess. See https://gumroad.com/l/1DChess
 
 START = "KQRBNP....pnbrqk"
 
+WHITE = "WHITE"
+BLACK = "BLACK"
+
+def color(char):
+	pass
+
 def legal_moves(board):
 	"""
 	Moves are represented as a (i, j) tuple, where the piece is moving from board[i] to board[j].
