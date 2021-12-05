e288b4fbc33c6280f291c01bd15a833f7fa8d402
Kevin Lacker <lacker@gmail.com>
2021-12-05 13:39:32 -0800

    eb7

--- python/chess1d.py
+++ python/chess1d.py
@@ -43,7 +43,7 @@ def legal_moves(board):
 		if piece == "P":
 			if empty(board, i+1):
 				moves.append((i, i+1))
-			if i == 5 and empty(board, 6) and empty(7):
+			if i == 5 and empty(board, 6) and empty(board, 7):
 				moves.append((5, 7))
 		elif piece == "p":
 			if empty(i-1):
