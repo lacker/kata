bcfc018ef026fce1382c0357ca7ce93e79250bf4
Kevin Lacker <lacker@gmail.com>
2022-12-14 10:14:47 -0800

    some structured style proofs

--- isabelle/Division.thy
+++ isabelle/Division.thy
@@ -1,7 +1,54 @@
 theory Division
   imports Main
 begin
-(* Let's get to the Division Theorem in the Isar style. *)
+(* Let's get to the Division Theorem in the "structured" style. *)
+
+datatype cnat = Zero | Suc cnat
+
+fun add :: "cnat \<Rightarrow> cnat \<Rightarrow> cnat" where
+"add Zero x = x" |
+"add (Suc x) y = Suc (add x y)"
+
+lemma add_zero_right [simp]: "add x Zero = x"
+proof (induct x)
+  case Zero show ?case by simp 
+next
+  case (Suc x) then show ?case by simp 
+qed
+
+lemma add_suc_right [simp]: "add x (Suc y) = Suc (add x y)"
+proof (induct x)
+  case Zero
+  then show ?case
+    by simp 
+next
+  case (Suc x)
+  then show ?case
+    by simp 
+qed
+
+lemma add_comm: "add x y = add y x"
+proof (induct x)
+  case Zero
+  then show ?case
+    by simp 
+next
+  case (Suc x)
+  then show ?case
+    by simp 
+qed
+
+lemma add_assoc: "add (add x y) z = add x (add y z)"
+proof (induct x)
+  case Zero
+  then show ?case
+    by simp 
+next
+  case (Suc x)
+  then show ?case
+    by simp
+qed
+
 
 
 
