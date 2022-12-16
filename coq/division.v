c478b9768115e235febacfd8e4d84ee449ca2caa
Kevin Lacker <lacker@gmail.com>
2022-12-16 12:21:01 -0800

    up to mul_assoc

--- coq/division.v
+++ coq/division.v
@@ -85,3 +85,75 @@ rewrite add_comm.
 trivial.
 Qed.
 
+Theorem mul_comm : forall (x : cnat) (y : cnat), mul x y = mul y x.
+intros x y.
+induction x.
+simpl.
+rewrite mul_zero_right.
+trivial.
+simpl.
+rewrite mul_suc_right.
+rewrite IHx.
+trivial.
+Qed.
+
+Theorem distrib : forall (x : cnat) (y : cnat) (z : cnat),
+  mul x (add y z) = add (mul x y) (mul x z).
+intros x y z.
+induction x.
+simpl.
+trivial.
+simpl.
+rewrite IHx.
+rewrite add_assoc.
+rewrite add_assoc.
+replace (add z (add (mul x y) (mul x z))) with (add (mul x y) (add z (mul x z))).
+trivial.
+rewrite <- add_assoc.
+rewrite <- add_assoc.
+replace (add (mul x y) z) with (add z (mul x y)).
+trivial.
+rewrite add_comm.
+trivial.
+Qed.
+
+Theorem mul_assoc : forall (x : cnat) (y : cnat) (z : cnat),
+  mul (mul x y) z = mul x (mul y z).
+intros x y z.
+induction x.
+simpl.
+trivial.
+simpl.
+rewrite mul_comm.
+rewrite distrib.
+rewrite <- IHx.
+rewrite mul_comm.
+replace (mul z (mul x y)) with (mul (mul x y) z).
+trivial.
+rewrite mul_comm.
+trivial.
+Qed.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
