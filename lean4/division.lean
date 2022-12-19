d0a051314c975f05befcd319f85506723cbb121c
Kevin Lacker <lacker@gmail.com>
2022-12-19 10:41:31 -0800

    at least defined some things

--- lean4/division.lean
+++ lean4/division.lean
@@ -1,1 +1,11 @@
-#eval Lean.versionString
+inductive Cnat where
+  | zero : Cnat
+  | succ : Cnat → Cnat
+
+def add (a b : Cnat) : Cnat :=
+  match a with
+  | Cnat.zero => b
+  | Cnat.succ c => Cnat.succ (add c b)
+
+theorem add_zero_right (x : Cnat) : add x Cnat.zero = x :=
+  sorry
