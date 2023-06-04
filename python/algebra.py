e54a20add74a9bbedc3b9b4d864d7328325e429f
Kevin Lacker <lacker@gmail.com>
2023-06-04 07:22:34 -0700

    Var

--- python/algebra.py
+++ python/algebra.py
@@ -2,9 +2,12 @@ class Formula:
     def __init__(self):
         pass
         
-class Atom:
+class Token:
     def __init__(self, token):
         self.token = token
         
     def __eq__(self, other):
         return self.token == other.token
+        
+class Variable:
+    pass
