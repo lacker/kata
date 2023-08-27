64724bab5bea55d661f5f58a4db1b269ffa3e88b
Kevin Lacker <lacker@gmail.com>
2023-08-27 13:59:26 -0700

    Update algebra.py

--- python/algebra.py
+++ python/algebra.py
@@ -55,10 +55,5 @@ def unify_var(n, term, var_map, term_map):
 
 def unify(left, right, left_map, right_map):
     if left.is_variable():
-        existing = left_map.get(left.number)
-        if existing is not None:
-            if existing == right:
-                return left_map, right_map
-            raise ValueError("cannot unify")
-        left_map[left.number] = right
-        return left_map, right_map
+        unify_var(left.number, right, left_map, right_map)
+        
