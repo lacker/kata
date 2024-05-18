44e02daad702675c158337144f8aa6783d246151
Kevin Lacker <lacker@gmail.com>
2024-05-18 08:30:51 -0700

    Update gen_problems.py

--- python/gen_problems.py
+++ python/gen_problems.py
@@ -26,6 +26,7 @@ class Tokenizer:
     def generate_token_pair():
         in_str, out_str = generate_str_pair()
         in_tokens = [self.to_token(ch) for ch in in_str]
+        out_tokens = [self.to_token(ch) for ch in out_str]
 
 def main():
     pass
