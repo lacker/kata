907c5f3c3f2c38111b22744953b45a203de50260
Kevin Lacker <lacker@gmail.com>
2025-07-02 15:24:12 -0700

    fetch url

--- effect/index.ts
+++ effect/index.ts
@@ -3,3 +3,10 @@ import { Effect, Console } from "effect";
 const program = Console.log("hello effect world!");
 
 Effect.runSync(program);
+
+// Fetch a URL in an Effect way
+const fetchUrl = (url: string) =>
+  Effect.tryPromise({
+    try: () => fetch(url).then(res => res.json()),
+    catch: (error) => new Error(`Failed to fetch: ${error}`)
+  });
