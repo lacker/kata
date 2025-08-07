31acdc66c28c77e287ea1a8d12d91dd604d8b78d
Kevin Lacker <lacker@gmail.com>
2025-08-07 16:17:12 -0700

    fn

--- zig/hello.zig
+++ zig/hello.zig
@@ -4,7 +4,11 @@ fn greet(comptime name: []const u8) []const u8 {
     return "Hello, " ++ name ++ "!";
 }
 
+fn printLine(msg: []const u8) !void {
+    try std.io.getStdOut().writer().print("{s}\n", .{msg});
+}
+
 pub fn main() !void {
     const msg = greet("world"); // evaluated entirely at compile time
-    try std.io.getStdOut().writer().print("{s}\n", .{msg});
+    printLine(msg);
 }
